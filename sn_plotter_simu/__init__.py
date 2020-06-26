9a3bb72d6f7e93506c09b3c71fbe70606670f178
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-06-26 10:09:20 +0200

    add plotters for simu and fit outputs

---
+++ sn_plotter_simu/__init__.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+filtercolors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['font.size'] = 12
+fontsize = 10
+plt.yticks(size=fontsize)
+plt.xticks(size=fontsize)
