aed3b31832cfc0c0314b1eacd2debe38f83c908c
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-06-26 10:19:07 +0200

    add setup.py file to the package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sn_plotters',
+    version='0.1',
+    description='Set of tools used to display SN pipeline results',
+    url='http://github.com/lsstdesc/sn_plotters',
+    author='Philippe Gris',
+    author_email='philippe.gris@clermont.in2p3.fr',
+    license='BSD',
+    packages=['sn_tools'],
+    python_requires='>=3.5',
+    zip_safe=False
+)
