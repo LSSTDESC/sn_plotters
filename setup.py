901ed894b3b9152a6b942487c813b35cef297e51
Philippe Gris <gris@clermont.in2p3.fr>
2020-06-30 13:12:14 +0200

    add sn_plotter_metrics as a package to install in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_plotter_simu', 'sn_plotter_fitlc'],
+    packages=['sn_plotter_simu', 'sn_plotter_fitlc','sn_plotter_metrics'],
     python_requires='>=3.5',
     zip_safe=False
 )
