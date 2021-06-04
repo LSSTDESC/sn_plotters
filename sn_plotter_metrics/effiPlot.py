6852df8b204fcd1170bb7e37ea1accc7d623b9f3
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-06-04 17:06:56 +0200

    some plot cleaning and (hopefully) improved

--- sn_plotter_metrics/effiPlot.py
+++ sn_plotter_metrics/effiPlot.py
@@ -8,6 +8,7 @@ from scipy.interpolate import interp1d
 
 from sn_tools.sn_rate import SN_Rate
 
+
 class plotEffi:
     """
     class to plot and process efficieicies from the nsn metric
@@ -22,11 +23,12 @@ class plotEffi:
       DD field to consider
 
     """
-    def __init__(self, dbDir,dbName,fieldName):
 
-        self.data = self.load(dbDir,dbName,fieldName)
-        
-    def load(self, dbDir,dbName,fieldName):
+    def __init__(self, dbDir, dbName, fieldName):
+
+        self.data = self.load(dbDir, dbName, fieldName)
+
+    def load(self, dbDir, dbName, fieldName):
         """
         Method to load the data
 
@@ -38,13 +40,13 @@ class plotEffi:
           OS to process
         fieldName: str
           DD field to consider
-     
+
         Returns
         -----------
         astropy table with efficiency data
 
         """
-        fName = '{}/{}/NSN_{}/*.hdf5'.format(dbDir,dbName,fieldName)
+        fName = '{}/{}/NSN_{}/*.hdf5'.format(dbDir, dbName, fieldName)
 
         fis = glob.glob(fName)
 
@@ -52,13 +54,13 @@ class plotEffi:
 
         keys = list(fFile.keys())
         data = Table()
-        
+
         for key in keys:
             tab = Table.read(fFile, path=key)
             data = vstack([data, tab])
 
         print(data)
-        
+
         return data
 
     def plotEffi(self):
@@ -71,18 +73,18 @@ class plotEffi:
             sel = self.data[idx]
             fig, ax = plt.subplots()
             for season in np.unique(sel['season']):
-                
-                self.plotEffi_indiv(sel,ax,healpixID,season,'effi', 'effi_err','Observing Efficiencies', ls='-',label='season {}'.format(season))
+
+                self.plotEffi_indiv(sel, ax, healpixID, season, 'effi', 'effi_err',
+                                    'Observing Efficiencies', ls='-', label='season {}'.format(season))
 
             ax.set_xlabel('z', weight='bold')
             ax.set_ylabel('Observing Efficiency', weight='bold')
             ax.legend()
-            ax.set_xlim([0.0,0.7])
+            ax.set_xlim([0.0, 0.7])
             ax.grid()
         plt.show()
 
-                            
-    def plotEffi_indiv(self, data,ax, healpixID, season ,vary='effi_err', erry=None, legy='', ls='None',sn_x1=-2.0,sn_color=0.2,lineColor='k',label=None):
+    def plotEffi_indiv(self, data, ax, healpixID, season, vary='effi_err', erry=None, legy='', ls='None', sn_x1=-2.0, sn_color=0.2, lineColor='k', label=None):
         """
         Simple method to plot vs z
 
@@ -116,47 +118,46 @@ class plotEffi:
         idx &= data['x1'] == sn_x1
         idx &= data['color'] == sn_color
         grp = data[idx]
-        
+
         yerr = None
 
-        
         if erry is not None:
-                yerr = grp[erry]
+            yerr = grp[erry]
 
         ax.errorbar(grp['z'], grp[vary], yerr=yerr,
                     marker='o', label=label, lineStyle=ls)
 
-    def getRates(self,survey_area=9.6):
+    def getRates(self, survey_area=9.6):
 
         rateSN = SN_Rate(H0=70., Om0=0.3,
-                              min_rf_phase=-15., max_rf_phase=30.)
-        
+                         min_rf_phase=-15., max_rf_phase=30.)
+
         # estimate the rates and nsn vs z
         zz, rate, err_rate, nsn, err_nsn = rateSN(zmin=0.01,
-                                                       zmax=1.,
-                                                       duration=180.,
-                                                       survey_area=survey_area,
-                                                       account_for_edges=True)
+                                                  zmax=1.,
+                                                  duration=180.,
+                                                  survey_area=survey_area,
+                                                  account_for_edges=True)
 
         # rate interpolation
         rateInterp = interp1d(zz, nsn, kind='linear',
                               bounds_error=False, fill_value=0)
         rateInterp_err = interp1d(zz, err_nsn, kind='linear',
-                                      bounds_error=False, fill_value=0)
-        
+                                  bounds_error=False, fill_value=0)
+
         return rateInterp, rateInterp_err
 
-    def get_zlims(self,data, ax,healpixID,season,sn_x1=-2.0,sn_color=0.2,zlim_coeff=0.95,zplot = np.arange(0.01,0.7,0.01)):
+    def get_zlims(self, data, ax, healpixID, season, sn_x1=-2.0, sn_color=0.2, zlim_coeff=0.95, zplot=np.arange(0.01, 0.7, 0.01)):
 
         rateInterp, rateInterp_err = self.getRates()
-        
+
         idx = data['healpixID'] == healpixID
         idx &= data['season'] == season
         idx &= data['x1'] == sn_x1
         idx &= data['color'] == sn_color
         effi = data[idx]
 
-        #interpolate efficiency vs z
+        # interpolate efficiency vs z
         effiInterp = interp1d(
             effi['z'], effi['effi'], kind='linear', bounds_error=False, fill_value=0.)
         # interpolate variance efficiency vs z
@@ -183,32 +184,32 @@ class plotEffi:
             zlimit = zlim(zlim_coeff).item()
             zlimit_minus = zlim_plus(zlim_coeff).item()
             zlimit_plus = zlim_minus(zlim_coeff).item()
-    
-        return nsn_cum_norm,zlimit
-    
-    def plotCumul(self,data, ax,healpixID,season,sn_x1=-2.0,sn_color=0.2,zlim_coeff=0.95,label=''):
-        zplot = np.arange(0.01,0.7,0.01)
-        nsn_cum_norm, zlimit = self.get_zlims(data, ax,healpixID,season,sn_x1=sn_x1,sn_color=sn_color,zlim_coeff=zlim_coeff,zplot=zplot)
-        ax.plot(zplot, nsn_cum_norm,label=label)
+
+        return nsn_cum_norm, zlimit
+
+    def plotCumul(self, data, ax, healpixID, season, sn_x1=-2.0, sn_color=0.2, zlim_coeff=0.95, label=''):
+        zplot = np.arange(0.01, 0.7, 0.01)
+        nsn_cum_norm, zlimit = self.get_zlims(
+            data, ax, healpixID, season, sn_x1=sn_x1, sn_color=sn_color, zlim_coeff=zlim_coeff, zplot=zplot)
+        ax.plot(zplot, nsn_cum_norm, label=label)
         zcomp = '$z_{complete}$ = '
-        ax.text(0.05,0.908-0.08*season,
-                zcomp+'{}'.format(np.round(zlimit,2)),fontsize=15,color=plt.gca().lines[-1].get_color())
-        ax.plot([zlimit]*2,[0.,0.95],color='k',ls='dashed')
-        #ax.fill_between(zplot, nsn_cum_norm-nsn_cum_norm_err,
+        ax.text(0.05, 0.908-0.08*season,
+                zcomp+'{}'.format(np.round(zlimit, 2)), fontsize=15, color=plt.gca().lines[-1].get_color())
+        ax.plot([zlimit]*2, [0., 0.95], color='k', ls='dashed')
+        # ax.fill_between(zplot, nsn_cum_norm-nsn_cum_norm_err,
         #              nsn_cum_norm+nsn_cum_norm_err, color='y')
 
-    def plotNSN(self,data, ax,healpixID,season,sn_x1=0.0,sn_color=0.0,zlim_coeff=0.95,label=''):
+    def plotNSN(self, data, ax, healpixID, season, sn_x1=0.0, sn_color=0.0, zlim_coeff=0.95, label=''):
 
-        
         rateInterp, rateInterp_err = self.getRates(survey_area=1.)
-        
+
         idx = data['healpixID'] == healpixID
         idx &= data['season'] == season
         idx &= data['x1'] == sn_x1
         idx &= data['color'] == sn_color
         effi = data[idx]
-        zplot = np.arange(0.01,0.71,0.01)
-        #interpolate efficiency vs z
+        zplot = np.arange(0.01, 0.71, 0.01)
+        # interpolate efficiency vs z
         effiInterp = interp1d(
             effi['z'], effi['effi'], kind='linear', bounds_error=False, fill_value=0.)
         # interpolate variance efficiency vs z
@@ -217,14 +218,23 @@ class plotEffi:
 
         nsn_cum = np.cumsum(effiInterp(zplot)*rateInterp(zplot))
 
-        ax.plot(zplot,nsn_cum,label=label)
+        ax.plot(zplot, nsn_cum, label=label)
 
-        nsn_cum_norm, zlimit = self.get_zlims(data, ax,healpixID,season,sn_x1=sn_x1,sn_color=sn_color,zlim_coeff=zlim_coeff,zplot=zplot)
-        zplotb = np.arange(0.01,zlimit,0.01)
+        nsn_cum_norm, zlimit = self.get_zlims(
+            data, ax, healpixID, season, sn_x1=sn_x1, sn_color=sn_color, zlim_coeff=zlim_coeff, zplot=zplot)
+        zplotb = np.arange(0.01, zlimit+0.005, 0.01)
         nsn_zlimit = np.cumsum(effiInterp(zplotb)*rateInterp(zplotb))[-1]
         print(nsn_zlimit)
-        zcomp = '$N_{SN}$ = '
-        ax.text(0.05,12-0.8*season,
-                zcomp+'{}'.format(int(nsn_zlimit)),fontsize=15,color=plt.gca().lines[-1].get_color())
+        zcomp = '$N_{SN}^{z\leq z_{complete}^{0.95}}$ = '
+        if season <= 5:
+            xpos = 0.1
+            ypos = 14.-2.*season
+        else:
+            xpos = 0.25
+            ypos = 14.-2.*(season-5)
 
-        
+        ax.text(xpos, ypos,
+                zcomp+'{}'.format(int(nsn_zlimit)), fontsize=15, color=plt.gca().lines[-1].get_color())
+
+        ax.plot([zlimit]*2, [0., nsn_zlimit], ls='dashed', color='k')
+        #ax.plot([0., zlimit], [nsn_zlimit]*2, ls='dashed', color='k')
