d35f8287a3b1737207ee5282d92f99ec8bc7b440
pgris <gris@clermont.in2p3.fr>
2023-06-13 13:12:24 +0200

    modif to install sn_plotter_analysis

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 # get the version here
-pkg_vars  = {}
+pkg_vars = {}
 
 with open("version.py") as fp:
     exec(fp.read(), pkg_vars)
@@ -13,7 +13,7 @@ setup(
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_plotter_simu', 'sn_plotter_fitlc','sn_plotter_metrics'],
+    packages=['sn_plotter_simu', 'sn_plotter_fitlc','sn_plotter_metrics','sn_plotter_analysis'],
     python_requires='>=3.5',
     zip_safe=False
 )
