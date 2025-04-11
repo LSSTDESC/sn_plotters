85d6e9d1fd7c0477233d6c2b387e41a709d97831
pgris <gris@clermont.in2p3.fr>
2025-04-11 17:26:01 +0200

    update

--- sn_plotter_cosmology/cosmoplot.py
+++ sn_plotter_cosmology/cosmoplot.py
@@ -349,7 +349,7 @@ def plot_allOS(resdf, config, dataCol='dbName_DD', configCol='dbName',
             leg = '_'.join(legs[:-1])
         if leg_prefix != '':
             leg = '{}{}'.format(leg_prefix, leg)
-        leg = row['dbNamePlot']
+        leg = row['dbName_plot']
         cosmo_plot(sel, varx=varx, legx=legx, vary=vary,
                    legy=legy, vary_std=vary_std, ax=ax, ls=row['ls'],
                    marker=row['marker'], color=row['color'],
