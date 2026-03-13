b6c14710295a9d1d9cdb44cabe3a879c4c250466
pgris <gris@clermont.in2p3.fr>
2026-03-13 09:18:56 +0100

    add some doc plus color tuning

--- sn_plotter_simu/plot_sn_simu.py
+++ sn_plotter_simu/plot_sn_simu.py
@@ -10,6 +10,7 @@ import numpy as np
 from astropy.table import Table
 from . import plt,filtercolors
 
+
 def plot_flux_spectra(sn_flux,sn_sed,outDir='None',bands='izy',phase_to_draw=[]):
     """
     Function to plot spectra (top) and flux from SN Ia.
@@ -22,7 +23,10 @@ def plot_flux_spectra(sn_flux,sn_sed,outDir='None',bands='izy',phase_to_draw=[])
         SN spectra.
     outDir: str, optional.
         Dir where to save data. The default is None
-
+   bands : str, optional
+        List of bands to consider. The default is 'izy'.
+    phase_to_draw : list(float), optional
+        List of phases to plot. The default is [].
     Returns
     -------
     None.
@@ -41,6 +45,7 @@ def plot_flux_spectra(sn_flux,sn_sed,outDir='None',bands='izy',phase_to_draw=[])
     figtitm = '(x1,color,z)=({},{},{}) \n'.format(sn_flux.meta['x1'],
                                               sn_flux.meta['color'],
                                               sn_flux.meta['z'])
+    lstyles = dict(zip(range(3),['solid','dashed','dotted']))
     for ip,phase in enumerate(phases):
         
         figtit = figtitm
@@ -48,7 +53,7 @@ def plot_flux_spectra(sn_flux,sn_sed,outDir='None',bands='izy',phase_to_draw=[])
         if not phase_to_draw:
             fig, ax = plt.subplots(nrows=2,figsize=(12,8))
         #ax1 = fig.add_subplot(2,1,1)
-        mjd = plot_sed(sn_sed,phase,figtit,fig=fig,ax=ax[0])
+        mjd = plot_sed(sn_sed,phase,figtit,fig=fig,ax=ax[0],lstyle=lstyles[ip])
         #ax1.set_xlim([0.,sed_max])
         mjda=9.*10**9
         label = True
@@ -69,7 +74,29 @@ def plot_flux_spectra(sn_flux,sn_sed,outDir='None',bands='izy',phase_to_draw=[])
     if phase_to_draw:
         plt.show()
 
-def plot_sed(sn_sed,phase,figtit,fig=None,ax=None):
+def plot_sed(sn_sed,phase,figtit,fig=None,ax=None,lstyle='solid'):
+    """
+    Function to plot SN Ia SED corresponding to a given phase
+
+    Parameters
+    ----------
+    sn_sed : pandas df
+        SN SED.
+    phase : float
+        phase of interest.
+    figtit : str
+        figure title.
+    fig : matplotlib figure, optional
+        figure for the plot. The default is None.
+    ax : matplotlib axis, optional
+        axis for the plot. The default is None.
+
+    Returns
+    -------
+    mjd : float
+        MJD corresponding to the phase value.
+
+    """
     
     idx = sn_sed['phase'] == phase
     sel_sed = Table(sn_sed[idx])
@@ -78,7 +105,9 @@ def plot_sed(sn_sed,phase,figtit,fig=None,ax=None):
     figtit += '{}'.format(mjd_str)
     fig.suptitle(figtit,fontweight='bold')
     ax.plot(sel_sed['wavelength']/10.,sel_sed['flux'],
-             color='k',marker='.',markersize=3,label='phase={}'.format(phase))
+             color='k',marker='None',
+             markersize=1,linestyle=lstyle,
+             label='phase={}'.format(phase))
     ax.grid(visible=True)
     ax.set_xlim([450.,1300.])
     ax.set_xlabel('wavelength [nm]')
@@ -90,6 +119,38 @@ def plot_sed(sn_sed,phase,figtit,fig=None,ax=None):
 def plot_flux(sn_flux,bands,mjd=9.10**9,
               varx='phase',legx='phase',
               fig=None,ax=None,labelIt=True):
+    """
+    Function to plot SN Ie flux
+
+    Parameters
+    ----------
+    sn_flux : pandas df
+        Data to plot.
+    bands : str
+        List of bands to consider.
+    mjd : float, optional
+        Max MJD for the plot. The default is 9.10**9.
+    varx : str, optional
+        x-axis variable. The default is 'phase'.
+    legx : str, optional
+        x-axis label. The default is 'phase'.
+    fig : matplotlib figure, optional
+        Figure for the plot. The default is None.
+    ax : matplotlib axis, optional
+        axis for the plot. The default is None.
+    labelIt : bool, optional
+        To label the plot. The default is True.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    mmark = ['h','v','s','o','P','X']
+    bands_all = 'ugrizy'
+    
+    markers = dict(zip(bands_all,mmark))
     
     
     if fig is None:
@@ -115,7 +176,8 @@ def plot_flux(sn_flux,bands,mjd=9.10**9,
         if not labelIt:
             thelabel=None
         ax.plot(sel_flux['phase'],sel_flux['flux'],
-                 color=filtercolors[b],label=thelabel)
+                 color=filtercolors[b],label=thelabel,
+                 marker=markers[b],mfc='None',markevery=10,markersize=10)
         ax.set_xlim([mjd_min,mjd_max])
         #ax2.set_ylim([flux_min[b],flux_max[b]])
         ax.set_ylim([flux_min,1.05*flux_max])
@@ -125,12 +187,27 @@ def plot_flux(sn_flux,bands,mjd=9.10**9,
     ax.set_ylabel('flux [pe/s]')
  
 def get_phase(df,phase_list):
+    """
+    Function to get the nearest phases in df wrt phase_list
+
+    Parameters
+    ----------
+    df : pandas df
+        data to process.
+    phase_list : list(float)
+        List of phases.
+
+    Returns
+    -------
+    r : list(float)
+        result.
+
+    """
     
     r = []
     for ph in phase_list:
         df['diff'] = np.abs(df['phase']-ph)
         idx = np.argmin(df['diff'])
-        print('ohohoh',ph,df[idx]['phase'])
         r.append(df[idx]['phase'])
         
     return r
